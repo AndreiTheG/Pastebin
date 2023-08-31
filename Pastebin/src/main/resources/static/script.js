15787d57aa3a7ed31a6a832240dd534c0549a106
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2023-08-31 15:45:21 +0300

    newCommit

--- Pastebin/src/main/resources/static/script.js
+++ Pastebin/src/main/resources/static/script.js
@@ -2,7 +2,5 @@ function input() {
     const valueInput = document.querySelector('textarea').value;
     if (valueInput.length == 0) {
         alert('You must enter some characters!');
-    } else {
-        console.log(valueInput);
     }
 }
